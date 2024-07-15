5682f32f76aa2701b3189967f5f63ed4af539013
Can Gündüz <cangunduz0001@gmail.com>
2024-07-15 23:52:04 +0300

    ecommerce v1.0.4

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterecom/pages/bottomnav.dart';
 import 'package:flutterecom/pages/login.dart';
 import 'package:flutterecom/pages/product_detail.dart';
 import 'package:flutterecom/pages/signup.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
